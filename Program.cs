70fb3e13a8e273378c98560ca041963f0e51290e
vincentogun <ogunbiyivincent@gmail.com>
2023-08-16 14:43:40 +0100

    First release

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+
+namespace SimpleCalculator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //DateTime.DaysInMonth(5, 7);
+        Console.WriteLine("What is your name??: ");
+        string name = "boye";
+        
+        Console.WriteLine("Welcome Mr " + name);
+        Console.WriteLine("What is your Birth Year?");
+        int age = 2023 - Convert.ToInt32(Console.ReadLine());
+
+        
+        Console.WriteLine("You are " + age +"yrs today. Happy Birthday Mr " + name);
+
+    }
+}
